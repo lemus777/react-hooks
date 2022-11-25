69499b3516b589a86158720d32b1e9bff8a88967
lemus <lemus777@gmail.com>
2022-11-25 18:51:48 +0100

    createContext y temas

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
+import { useState } from 'react';
 import { Header } from './components/Header.jsx';
 import { Characters } from './components/Characters.jsx';
+import { ThemeContext } from './context/ThemeContext.js';
 import './App.css';
 
 function App() {
+  const [theme, setTheme] = useState('bg-light');
+
   return (
-    <div className="App">
-      <Header />
-      <Characters />
-    </div>
+    <ThemeContext.Provider value={{ theme, setTheme }}>
+      <div className={'App ' + theme}>
+        <Header />
+        <Characters />
+      </div>
+    </ThemeContext.Provider>
+    
   );
 }
 
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,16 +1,19 @@
-import { useState } from "react";
+import { useState, useContext } from "react";
+import { ThemeContext } from "../context/ThemeContext";
 
 const Header = () => {
   const [darkMode, setDarkMode] = useState(false);
+  const { theme, setTheme } = useContext(ThemeContext);
 
   const handleClick = () => {
     setDarkMode(!darkMode);
+    theme === 'bg-light' ? setTheme('bg-dark') : setTheme('bg-light');
   }
 
   return (
     <div className="Header">
       <h1>React Hooks</h1>
-      <button type='button' onClick={handleClick}>{darkMode ? 'Dark mode' : 'Light mode'}</button>
+      <button type='button' onClick={handleClick} className='buttonToggle'>{darkMode ? 'Light mode' : 'Dark mode'}</button>
       {/*<button type='button' onClick={() => setDarkMode(!darkMode)}>{darkMode ? 'Dark mode 2' : 'Light mode 2'}</button>*/}
     </div>
   );
